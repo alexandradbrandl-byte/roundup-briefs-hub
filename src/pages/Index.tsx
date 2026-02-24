66d1cac12fca61132506f5e0b8aed83acab34a0c
alexandradbrandl-byte <alexandra.d.brandl@gmail.com>
2026-02-24 20:40:28 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
 import Masthead from "@/components/Masthead";
 import FilterBar from "@/components/FilterBar";
 import ArticleGrid from "@/components/ArticleGrid";
 import SiteFooter from "@/components/SiteFooter";
-import AboutPage from "@/components/AboutPage";
-import ScrollButtons from "@/components/ScrollButton";
+import ScrollButton from "@/components/ScrollButton";
 import { useArticles } from "@/hooks/useArticles";
 
 const Index = () => {
-  const [showAbout, setShowAbout] = useState(false);
   const {
     articles,
     stats,
@@ -23,38 +20,28 @@ const Index = () => {
 
   return (
     <div className="min-h-screen bg-background">
-      <Masthead
-        stats={stats}
-        showAbout={showAbout}
-        onAboutToggle={() => setShowAbout((v) => !v)}
-      />
+      <Masthead stats={stats} />
 
-      {showAbout ? (
-        <AboutPage />
-      ) : (
-        <>
-          <FilterBar
-            filters={filters}
-            setFilters={setFilters}
-            sources={sources}
-            articleCount={articles.length}
-            isFiltered={isFiltered}
-            clearFilters={clearFilters}
-          />
-          <div className="pt-4 pb-8">
-            <ArticleGrid
-              articles={articles}
-              loading={loading}
-              error={error}
-              isFiltered={isFiltered}
-              clearFilters={clearFilters}
-            />
-          </div>
-        </>
-      )}
+      <FilterBar
+        filters={filters}
+        setFilters={setFilters}
+        sources={sources}
+        articleCount={articles.length}
+        isFiltered={isFiltered}
+        clearFilters={clearFilters}
+      />
+      <div className="pt-4 pb-8">
+        <ArticleGrid
+          articles={articles}
+          loading={loading}
+          error={error}
+          isFiltered={isFiltered}
+          clearFilters={clearFilters}
+        />
+      </div>
 
       <SiteFooter />
-      <ScrollButtons />
+      <ScrollButton />
     </div>
   );
 };
