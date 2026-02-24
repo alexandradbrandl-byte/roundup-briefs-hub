c225a157d8076b5c4738238643efbebc3ed7ad73
alexandradbrandl-byte <alexandra.d.brandl@gmail.com>
2026-02-24 20:39:20 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import ThemenPage from "./pages/ThemenPage";
+import NewsletterPage from "./pages/NewsletterPage";
+import UeberUnsPage from "./pages/UeberUnsPage";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -16,7 +19,9 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/themen" element={<ThemenPage />} />
+          <Route path="/newsletter" element={<NewsletterPage />} />
+          <Route path="/ueber-uns" element={<UeberUnsPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
