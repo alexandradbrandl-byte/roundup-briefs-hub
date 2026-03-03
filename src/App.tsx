0e041ddd741378df8f791ce6c4cd0a317f4191e3
alexandradbrandl-byte <alexandra.d.brandl@gmail.com>
2026-03-03 18:29:09 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import AnalysisPage from "./pages/AnalysisPage";
+import MapPage from "./pages/MapPage";
+import FeedPage from "./pages/FeedPage";
+import ThemenPage from "./pages/ThemenPage";
 import NewsletterPage from "./pages/NewsletterPage";
 import UeberUnsPage from "./pages/UeberUnsPage";
 import NotFound from "./pages/NotFound";
@@ -18,10 +19,14 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/analysis" element={<AnalysisPage />} />
+          <Route path="/" element={<MapPage />} />
+          <Route path="/de" element={<FeedPage country="de" countryName="Deutschland" />} />
+          <Route path="/at" element={<FeedPage country="at" countryName="Österreich" />} />
+          <Route path="/ch" element={<FeedPage country="ch" countryName="Schweiz" />} />
+          <Route path="/themen" element={<ThemenPage />} />
           <Route path="/newsletter" element={<NewsletterPage />} />
           <Route path="/ueber-uns" element={<UeberUnsPage />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
